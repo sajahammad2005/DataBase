16582611a929e2b0a3624452eb5a31cfc80fc1e7
sajah <sajah@DESKTOP-FF9JFND>
2026-01-15 16:33:00 +0200

    Initial Commit

---
+++ ProjectDatabase/src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import java.sql.Connection;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	static Connection conn = DBConnect.getConnection();
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			 if (conn != null) {
+		            System.out.println("Connected!");
+		        } else {
+		            System.out.println("X");
+		        }
+			primaryStage.setScene(new CustomerScene(primaryStage).createScene());
+			primaryStage.setTitle("Customer Stage");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+//
